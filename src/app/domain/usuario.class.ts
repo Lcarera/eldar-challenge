6505f9dcea8bb7ee79d58ad6f66101231a782539
lcarera <lcarera04@gmail.com>
2024-10-15 14:19:51 -0300

    dominio

---
+++ src/app/domain/usuario.class.ts
@@ -0,0 +1,14 @@
+import { Rol } from "./rol.enum";
+
+export class Usuario {
+  constructor(
+    public id: number,
+    public email: string,
+    public password: string,
+    public role: Rol
+  ) {}
+
+  isAdmin(): boolean {
+    return this.role === Rol.ADMIN;
+  }
+}
